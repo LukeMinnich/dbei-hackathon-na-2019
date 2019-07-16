a7f3817af2c39abb409f95bb90339193c25a70f3
Anthonie Kramer <anthonie.kramer@katerra.com>
2019-07-15 18:53:39 -0700

    testing out algorithm

---
+++ src/BuildingLayout/BuildingUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BuildingLayout
+{
+    public class BuildingUnit
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double DoorCenterLocation { get; set; }
+        public double DoorWidth { get; set; }
+        
+        public double Area()
+        {
+            return Width * Depth;
+        }
+
+        public BuildingUnit(string name, string type, double width, double depth, double doorCenter, double doorWidth)
+        {
+            Name = name;
+            Type = type;
+            Width = width;
+            Depth = depth;
+            DoorCenterLocation = doorCenter;
+            DoorWidth = doorWidth;
+        }
+
+
+    }
+}
---
+++ src/BuildingLayout/BuildingUnitMix.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingLayout
+{
+    public class BuildingUnitMix : Dictionary<string, double>
+    {
+    }
+}
